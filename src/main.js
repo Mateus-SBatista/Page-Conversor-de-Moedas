146017f13a94c1f5485eca484e8f0ac3f9e32108
Mateus-SBatista <94994088+Mateus-SBatista@users.noreply.github.com>
2024-01-23 15:09:01 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,42 +3,42 @@ function converterDolar() {
     var valor = valorElemento1.value;
     var valorNumDolar = parseFloat(valor);
   
-    var valorReal = valorNumDolar * 4.92;
+    var valorReal = valorNumDolar * 4.97;
     console.log(valorReal);
   
-    var dolarConvertido = document.getElementById("valorConvertidoDolar");
+    var dolarConvertido = document.getElementById("valorConvertido");
   
-    var valorConvertido = "Seu valor em Real em Dolar agora é R$ " + valorReal.toFixed(2);
+    var valorConvertido = "Seu valor de Dolar em Real agora é R$ =" + valorReal;
   
     dolarConvertido.innerHTML = valorConvertido;
-}
-
-function converterEuro() {
+  }
+  
+  function converterEuro() {
     var valorElemento2 = document.getElementById("valor");
     var valor = valorElemento2.value;
     var valorNumEuro = parseFloat(valor);
   
-    var valorReal2 = valorNumEuro * 5.36;
+    var valorReal2 = valorNumEuro * 5.38;
     console.log(valorReal2);
   
-    var euroConvertido = document.getElementById("valorConvertidoEuro");
+    var euroConvertido = document.getElementById("valorConvertido");
   
-    var valorEuroConvertido = "Seu valor em Real em Euro agora é R$ " + valorReal2.toFixed(2);
+    var valorEuroConvertido = "Seu valor Euro em Real agora é R$ =" + valorReal2;
   
     euroConvertido.innerHTML = valorEuroConvertido;
-}
-
-function converterBitcoin() {
+  }
+  
+  function converterBitcoin() {
     var valorElemento3 = document.getElementById("valor");
     var valor = valorElemento3.value;
     var valorNumBc = parseFloat(valor);
   
-    var valorReal3 = valorNumBc * 202.453;
+    var valorReal3 = valorNumBc * 195.957;
     console.log(valorReal3);
   
-    var bcConvertido = document.getElementById("valorConvertidoBitcoin");
+    var bcConvertido = document.getElementById("valorConvertido");
   
-    var valorBcConvertido = "Seu valor em Real em Bitcoin agora é R$ " + valorReal3.toFixed(2);
+    var valorBcConvertido = "Seu valor Bitcoin em Real agora é R$ =" + valorReal3;
   
     bcConvertido.innerHTML = valorBcConvertido;
-}
+  }
